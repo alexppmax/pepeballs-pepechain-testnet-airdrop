b22326a101d30564858e2641d9ab89265be80207
Drikus Roor <drikusroor@gmail.com>
2023-05-13 15:23:48 +0200

    Configure VT323 font globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,14 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
 
-// 1. Import the extendTheme function
-import { extendTheme } from "@chakra-ui/react";
+import Fonts from "../styles/chakra/fonts";
 
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
+import theme from "../styles/chakra/theme";
 
-export const theme = extendTheme({ colors });
-
-// 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   );
