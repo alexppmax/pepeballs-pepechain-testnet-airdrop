03e7c4bac371b0d3a6f0f243f05d142e979aa75a
Alex <strafalogea.alexandru@gmail.com>
2023-05-13 23:50:46 +0200

    background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import App from "../components/App";
+import Navbar from "../components/Navbar";
+import { Container } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -12,6 +14,9 @@ export default function Home() {
           content="Start your Pepechain Testnet journey by participating in on-chain quests!"
         />
         <link rel="icon" href="/favicon.ico" />
+        <Navbar />
+        <div className={styles.background}></div>
+        <Container maxW="container.md" py={8}></Container>
       </Head>
       <App />
       <main className={styles.main}></main>
